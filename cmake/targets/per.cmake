f3bd12e4d11314b4cc08e723e80ec6f4bbc5ce3f
Michael <m.aichmueller@gmail.com>
2021-10-31 14:05:52 +0000

    wip

--- cmake/targets/per.cmake
+++ cmake/targets/per.cmake
@@ -1,46 +1,31 @@
-cmake_minimum_required(VERSION 3.16)
 
 set(LIBRARY_SOURCES
         experience_replay.cpp
         )
 list(TRANSFORM LIBRARY_SOURCES PREPEND "${PROJECT_PER_SRC_DIR}/")
 
-add_library(${per_lib}_interface INTERFACE)
-add_library(${per_lib}_static STATIC)
-add_library(${per_lib}_shared SHARED)
+add_library(${per_lib} ${per-lib-type})
 
-set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -Wall")
+target_sources(${per_lib} PRIVATE ${LIBRARY_SOURCES})
 
-target_sources(${per_lib}_static PRIVATE ${LIBRARY_SOURCES})
-target_sources(${per_lib}_shared PRIVATE ${LIBRARY_SOURCES})
-
-target_include_directories(${per_lib}_interface
-        INTERFACE
+target_include_directories(${per_lib}
+        PUBLIC
         $<BUILD_INTERFACE:${PROJECT_PER_INCLUDE_DIR}>
-        $<INSTALL_INTERFACE:include>
+        $<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>
         )
 
-target_link_libraries(${per_lib}_interface
-        INTERFACE
+target_link_libraries(${per_lib}
+        PRIVATE
+        project_warnings
+        PUBLIC
         project_options
         pybind11::module
-        $<$<NOT:$<BOOL:USE_PYBIND11_FINDPYTHON>>:Python3::Module >
+        $<$<NOT:$<BOOL:USE_PYBIND11_FINDPYTHON>>:Python3::Module>
         )
 
-target_link_libraries(${per_lib}_static
-        PRIVATE
-        project_warnings
-        PUBLIC
-        ${per_lib}_interface
-        )
-
-target_link_libraries(${per_lib}_shared
-        PRIVATE
-        project_warnings
-        PUBLIC
-        ${per_lib}_interface
-        )
-set_target_properties(${per_lib}_shared
+set_target_properties(${per_lib}
         PROPERTIES
         CXX_VISIBILITY_PRESET hidden
+        $<$<PLATFORM_ID:WIN32>:MSVC_RUNTIME_LIBRARY ${CMAKE_MSVC_RUNTIME_LIBRARY}>
         )
+
--- cmake/targets/pyper.cmake
+++ cmake/targets/pyper.cmake
@@ -14,7 +14,7 @@ endif ()
 pybind11_add_module(${per_pymodule} MODULE ${_pyper_exclude_from_all} ${LIBRARY_SOURCES} ${PYTHON_MODULE_SOURCES})
 
 set_target_properties(${per_pymodule} PROPERTIES
-        LIBRARY_OUTPUT_NAME _pyper
+        LIBRARY_OUTPUT_NAME _${per_pymodule}
         CXX_VISIBILITY_PRESET hidden
         )
-target_link_libraries(${per_pymodule} PUBLIC ${per_lib}_shared)
+target_link_libraries(${per_pymodule} PUBLIC ${per_lib})
