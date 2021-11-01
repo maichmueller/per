9791c4b8f519b7da1e777e1636dbb9ceb7cfbcda
Michael <m.aichmueller@gmail.com>
2021-11-01 12:56:32 +0000

    wip

--- cmake/targets/per.cmake
+++ cmake/targets/per.cmake
@@ -6,26 +6,41 @@ list(TRANSFORM LIBRARY_SOURCES PREPEND "${PROJECT_PER_SRC_DIR}/")
 
 add_library(${per_lib} ${per-lib-type})
 
-target_sources(${per_lib} PRIVATE ${LIBRARY_SOURCES})
+target_sources(
+        ${per_lib}
+        PRIVATE
+        ${LIBRARY_SOURCES}
+)
 
-target_include_directories(${per_lib}
+target_include_directories(
+        ${per_lib}
         PUBLIC
         $<BUILD_INTERFACE:${PROJECT_PER_INCLUDE_DIR}>
         $<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>
-        )
+)
 
-target_link_libraries(${per_lib}
+target_link_libraries(
+        ${per_lib}
         PRIVATE
         project_warnings
         PUBLIC
         project_options
         pybind11::module
         $<$<NOT:$<BOOL:USE_PYBIND11_FINDPYTHON>>:Python3::Module>
-        )
+)
 
-set_target_properties(${per_lib}
+set_target_properties(
+        ${per_lib}
         PROPERTIES
         CXX_VISIBILITY_PRESET hidden
         $<$<PLATFORM_ID:WIN32>:MSVC_RUNTIME_LIBRARY ${CMAKE_MSVC_RUNTIME_LIBRARY}>
-        )
+)
 
+target_compile_options(
+        ${per_lib}
+        PRIVATE
+        $<${msvc-rt-mtd-static}:-MTd>
+        $<${msvc-rt-mt-static}:-MT>
+        $<${msvc-rt-mtd-dll}:-MDd>
+        $<${msvc-rt-mt-dll}:-MD>
+)
