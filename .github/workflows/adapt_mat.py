830203e2963cd8f1b5b05d4b4600dbdf39994367
der4twu <m.aichmueller@gmail.com>
2021-12-22 12:13:55 +0100

    bug fixes for interface lib

--- .github/workflows/adapt_mat.py
+++ .github/workflows/adapt_mat.py
@@ -1,7 +1,6 @@
 import os
 import json
 from copy import deepcopy
-from pprint import pprint
 
 d = []
 with open(os.path.join(os.getcwd(), '.github', 'workflows', 'build-matrix.json')) as json_data:
@@ -10,13 +9,13 @@ with open(os.path.join(os.getcwd(), '.github', 'workflows', 'build-matrix.json')
 new_data = []
 for entry in d:
     entry_copy1 = deepcopy(entry)
-    entry_copy1['shared'] = "FALSE"
-    entry_copy1['libtype'] = 'static'
+    # entry_copy1['shared'] = "FALSE"
+    # entry_copy1['libtype'] = 'static'
     new_data.append(entry_copy1)
-    entry_copy2 = deepcopy(entry_copy1)
-    entry_copy2['shared'] = "TRUE"
-    entry_copy2['libtype'] = 'dynamic'
-    new_data.append(entry_copy2)
+    # entry_copy2 = deepcopy(entry_copy1)
+    # entry_copy2['shared'] = "TRUE"
+    # entry_copy2['libtype'] = 'dynamic'
+    # new_data.append(entry_copy2)
 
 with open(os.path.join(os.getcwd(), '.github', 'workflows', 'build-matrix.json'), 'w') as f:
     json.dump(new_data, f)
