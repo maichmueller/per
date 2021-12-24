9bf0f3a7d4c80417f058da339701a92b05661ab1
der4twu <m.aichmueller@gmail.com>
2021-12-24 17:37:08 +0100

    change output dir

--- cmake/settings/Doxygen.cmake
+++ cmake/settings/Doxygen.cmake
@@ -4,6 +4,8 @@ set(DOXYGEN_CALL_GRAPH YES)
 set(DOXYGEN_EXTRACT_ALL YES)
 set(DOXYGEN_RECURSIVE YES)
 set(DOXYGEN_USE_MATHJAX YES)
+set(DOXYGEN_GENERATE_XML YES)
+set(DOXYGEN_OUTPUT_DIR ${_docs_OUTPUT_DIR})
 find_package(Doxygen REQUIRED dot)
 doxygen_add_docs(
         doxygen-docs
